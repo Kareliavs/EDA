0804edb6b58eb21a97084de5f4e2d4d51c7759ec
Kareliavs <Kareliavs@users.noreply.github.com>
2016-08-27 09:40:04 -0500

    Update Lista_invertida.cc

--- Lista_invertida.cc
+++ Lista_invertida.cc
@@ -1,19 +1,19 @@
 #include <iostream>
 
 using namespace std;
-
+template<class en>
 class Nodo
 {
 public:
-    int m_dato;
+    en m_dato;
     Nodo* sgt;
-    Nodo(int dato)
+    Nodo(en dato)
     {
         m_dato=dato;
         sgt=NULL;
     }
 };
-
+template<class Nodo>
 class Lista
 {
 public:
@@ -76,13 +76,42 @@ public:
         }
         return 0;
     }
+    bool eliminar(int dato)
+    { Nodo* aux = m_head;
+      Nodo* aux2=m_head;
+        //cout<<"Estoy eliminando"<<endl;
+        if(buscar(dato))
+        {
+            while(aux->m_dato!=dato)//return 1;
+            {
+
+                if(aux !=m_head )
+                {
+                    aux2=aux2->sgt;
+                }
+                aux=aux->sgt;
+            }
+            //cout<<"Lo busque"<<aux->m_dato<<" this is su previo"<<aux2->m_dato<<endl;
+            //aux->sgt= NULL;
+              if(aux==m_head)
+                {   //cout<<"es cabeza"<<endl;
+                    m_head=aux->sgt;
+                    aux=NULL;
+                }
+              else aux2->sgt=aux->sgt;
+
+
+        }
+
+        else cout<<"No existe"<<endl;
+    }
 };
 
-
-
-Lista a;int x, num;
+Lista<Nodo<char>> a;
+int x;
+char num;
 int main()
-{
+{   //stem("clear");
 
 
     cout<<endl;
@@ -92,25 +121,41 @@ int main()
     cout<<"2 Invertir"<<endl;
     cout<<"3 Buscar"<<endl;
     cout<<"4 Insertar"<<endl;
+    cout<<"5 Eliminar"<<endl;
     cout<<"----------------------------"<<endl;
      cin>>x;
      if(x==1)
      {  cout<<"----------------------------"<<endl;
         a.imprimir();
      }
-     else if(x==2)a.invertir();
+     else if(x==2)
+        {
+            a.invertir();
+            system("clear");
+        }
      else if(x==3)
         {cout<<"ingrese numero a buscar"<<endl;
         cin>>num;
         if(a.buscar(num))cout<<"si esta"<<endl;
         else cout<<"no esta"<<endl;
-         }
+        system("clear");
+        }
      else if(x==4)
         {cout<<"ingrese numero a insertar"<<endl;
         cin>>num;
         a.insertar(num);
+        system("clear");
+        }
+     else if(x==5)
+        {cout<<"ingrese numero a eliminar"<<endl;
+        cin>>num;
+        a.eliminar(num);
+        system("clear");
         }
     else cout<<"Elija opcion valida";
+   // a.eliminar(4);
+
     main();
 return 0;
 }
+
